c2bc4f55603d24d58bf56d9bc976f95eb7366753
ACD-MD <ashquotes111@gmail.com>
2025-01-03 00:01:19 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,8 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID ||
-"6ZVjXDoa#DTKoAEU2xeUbjOMKDL2W9v0nwvdV99E5pxRpp-V8iTk",
+SESSION_ID: process.env.SESSION_ID || "SA1UEapL#OJbym7y_h7YbmMyeo1zmT99sxneXfCmzbT7dqYgk_5A",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/LP1dXT2/IMG-20241208-WA0281.jpg",
 ALIVE_MSG:process.env.ALIVE_MSG || "*👋🏻 ʜᴇʟʟᴏᴡ ${pushname}, ɪ ᴀᴍ ᴀʟɪᴠᴇ ɴᴏᴡ* 📍",
 };
